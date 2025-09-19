ba22f8bea734a5e980ed71ade252fae4715714ee
nghia1501 <nghianguyen15012004@gmail.com>
2025-09-19 22:32:57 +0700

    config project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text, View } from '../../components/Themed';
+import DeatailHeader from '../../components/layout/DetailHeader';
 
-export default function TabTwoScreen() {
+export default function TabOneScreen() {
   return (
+
+
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <DeatailHeader title='Detail Header'></DeatailHeader>
+      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
